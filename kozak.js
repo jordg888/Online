19154a2d260e4c39b64701ae48368b7736bfac24
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-16 18:34:15 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -2,28 +2,24 @@
     'use strict';
 
     function KozakTiv() {
-        // Можеш замінити це посилання на власну іконку (SVG або PNG)
         var ICON_KOZAK = 'https://yarikrazor-star.github.io/lmp/wiki.svg'; 
         var api_proxy = 'https://vercel-proxy-blue-six.vercel.app/api?url=';
 
         this.init = function () {
             var _this = this;
-            // Слухаємо подію завершення рендеру картки фільму
             Lampa.Listener.follow('full', function (e) {
                 if (e.type === 'complite') {
                     _this.cleanup();
-                    // Невелика затримка для стабільності
                     setTimeout(function() {
                         try {
                             _this.render(e.data, e.object.activity.render());
                         } catch (err) {
-                            console.log('Kozak: Error rendering button', err);
+                            console.log('Kozak: Render error', err);
                         }
                     }, 200);
                 }
             });
             
-            // Реєструємо компонент для відображення результатів
             Lampa.Component.add('kozak_search', _this.component);
         };
 
@@ -36,24 +32,21 @@
             var container = $(html);
             if (container.find('.lampa-kozak-button').length) return;
 
-            // Створюємо кнопку
+            // ВИПРАВЛЕНО: Додано текст "Козак ТВ" і стиль, щоб його було видно
             var button = $('<div class="full-start__button selector lampa-kozak-button">' +
-                                '<img src="' + ICON_KOZAK + '" style="width: 1.6em; height: 1.6em; margin-right: 5px; object-fit: contain;">' +
+                                '<img src="' + ICON_KOZAK + '" style="width: 1.6em; height: 1.6em; margin-right: 10px; object-fit: contain;">' +
                                 '<span>Козак ТВ</span>' +
                             '</div>');
 
-            // Шукаємо контейнер для кнопок
             var buttons_container = container.find('.full-start-new__buttons, .full-start__buttons');
-            
-            // Вставляємо кнопку (наприклад, після другої існуючої кнопки)
             var neighbors = buttons_container.find('.selector');
+            
             if (neighbors.length >= 2) {
                 button.insertAfter(neighbors.eq(1));
             } else {
                 buttons_container.append(button);
             }
 
-            // Дія при натисканні
             button.on('hover:enter click', function() {
                 Lampa.Activity.push({
                     title: 'Козак ТВ',
@@ -64,7 +57,6 @@
             });
         };
 
-        // Компонент, який відкривається при натисканні на кнопку
         this.component = function(object) {
             var network = new Lampa.Reguest();
             var scroll = new Lampa.Scroll({mask: true, over: true});
@@ -74,35 +66,54 @@
                 var _this = this;
                 var title = object.movie.title || object.movie.name;
                 
-                // Твій запит через Vercel
+                // Використовуємо VideoCDN API для пошуку
                 var search_url = 'https://videocdn.tv/api/movies?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=' + encodeURIComponent(title);
                 var final_url = api_proxy + encodeURIComponent(search_url);
 
-                Lampa.Select.show({title: 'Шукаємо на Козак ТВ...'});
+                Lampa.Select.show({title: 'Пошук на Козак ТВ...'});
 
                 network.silent(final_url, function(json) {
                     Lampa.Select.close();
-                    if (json.data && json.data.length > 0) {
+                    
+                    // ВИПРАВЛЕНО: Перевірка, чи прийшов масив, щоб не було помилки forEach
+                    if (json && json.data && Array.isArray(json.data) && json.data.length > 0) {
                         json.data.forEach(function(item) {
-                            var card = Lampa.Template.get('button', {title: item.title});
+                            var card = Lampa.Template.get('button', {
+                                title: item.title,
+                                description: item.year || ''
+                            });
+                            
                             card.on('hover:enter', function() {
-                                Lampa.Noty.show('Знайдено ID: ' + item.id);
-                                // Тут можна викликати плеєр
+                                // Плеєр: відкриваємо iframe від VideoCDN
+                                if (item.iframe_src) {
+                                    Lampa.Player.play({
+                                        url: 'https:' + item.iframe_src,
+                                        title: item.title
+                                    });
+                                } else {
+                                    Lampa.Noty.show('Відео недоступне');
+                                }
                             });
                             files.append(card);
                         });
                     } else {
+                        // Якщо нічого не знайдено або помилка формату
+                        Lampa.Noty.show('Нічого не знайдено');
                         files.append(Lampa.Template.get('empty'));
                     }
-                    scroll.append(files.render());
+                    _this.loading(false);
                 }, function() {
                     Lampa.Select.close();
-                    Lampa.Noty.show('Помилка проксі');
+                    Lampa.Noty.show('Помилка сервера Vercel');
                     files.append(Lampa.Template.get('empty'));
-                    scroll.append(files.render());
+                    _this.loading(false);
                 });
 
                 return scroll.render();
+            };
+
+            this.loading = function(status) {
+                // Пуста функція для сумісності
             };
 
             this.render = function() {
@@ -111,7 +122,6 @@
         };
     }
 
-    // Запуск плагіна
     if (window.Lampa) {
         new KozakTiv().init();
     }
