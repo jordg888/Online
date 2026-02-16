578e2c4e866d3016bd71c03159fc01f71acc3a1e
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-16 21:23:54 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -23,86 +23,86 @@
             container.append(button);
 
             button.on('hover:enter click', function() {
-                var movie = data.movie;
-                var title = movie.title || movie.name;
-
-                // Створюємо компонент з табами зверху
-                Lampa.Component.add('kozak_mod', function(object) {
-                    var network = new Lampa.Reguest();
-                    var scroll = new Lampa.Scroll({mask: true, over: true});
-                    var files = new Lampa.Explorer(object);
-                    var results = [];
-                    
-                    this.create = function() {
-                        var _this = this;
-                        // Вибір балансерів зверху
-                        var filter = new Lampa.Filter(object);
-                        filter.render().find('.filter__sort').remove(); // Прибираємо сортування
-                        
-                        filter.onSelect = function(item) {
-                            _this.search(item.source);
-                        };
-
-                        filter.set('source', [
-                            {title: 'Ashdi (UA)', source: 'ashdi', selected: true},
-                            {title: 'VideoCDN', source: 'vcdn'},
-                            {title: 'Резерв', source: 'rezerv'}
-                        ]);
-
-                        this.search('ashdi'); // Стартуємо з Ashdi
-                        
-                        scroll.append(filter.render());
-                        scroll.append(files.render());
-                        return scroll.render();
-                    };
-
-                    this.search = function(source_type) {
-                        files.clear();
-                        Lampa.Select.show({title: 'Шукаємо...'});
-                        
-                        var base_url = '';
-                        if(source_type === 'ashdi') base_url = 'https://ashdi.vip/api/video?title=';
-                        else if(source_type === 'vcdn') base_url = 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
-                        else base_url = 'https://videocdn.tv/api/movies?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
-
-                        var final_url = api_proxy + encodeURIComponent(base_url + encodeURIComponent(title));
-
-                        network.silent(final_url, function(json) {
-                            Lampa.Select.close();
-                            var items = json.data || (Array.isArray(json) ? json : []);
-                            
-                            if (items.length > 0) {
-                                items.forEach(function(item) {
-                                    var card = Lampa.Template.get('button', {title: item.title || item.name || 'Відео'});
-                                    card.on('hover:enter', function() {
-                                        var v_url = item.iframe_src || item.url;
-                                        if (v_url) {
-                                            if (!v_url.startsWith('http')) v_url = 'https:' + v_url;
-                                            Lampa.Player.play({ url: v_url, title: item.title || title });
-                                        }
-                                    });
-                                    files.append(card);
-                                });
-                            } else {
-                                Lampa.Noty.show('Нічого не знайдено на цьому джерелі');
-                            }
-                        }, function() {
-                            Lampa.Select.close();
-                            Lampa.Noty.show('Помилка сервера');
-                        });
-                    };
-
-                    this.render = function() { return this.create(); };
-                });
-
                 Lampa.Activity.push({
                     title: 'Козак ТВ',
                     component: 'kozak_mod',
-                    movie: movie,
+                    movie: data.movie,
                     page: 1
                 });
             });
         };
+
+        Lampa.Component.add('kozak_mod', function(object) {
+            var network = new Lampa.Reguest();
+            var scroll = new Lampa.Scroll({mask: true, over: true});
+            var files = new Lampa.Explorer(object);
+            var filter = new Lampa.Filter(object);
+            var title = object.movie.title || object.movie.name;
+            
+            this.create = function() {
+                var _this = this;
+
+                // Налаштування вибору балансерів зверху
+                filter.onSelect = function(item) {
+                    _this.search(item.source);
+                };
+
+                filter.set('source', [
+                    {title: 'Ashdi (UA)', source: 'ashdi', selected: true},
+                    {title: 'VideoCDN', source: 'vcdn'},
+                    {title: 'Резерв', source: 'rezerv'}
+                ]);
+
+                filter.render().find('.filter__sort').remove(); // Видаляємо непотрібне сортування
+                
+                scroll.append(filter.render());
+                scroll.append(files.render());
+
+                this.search('ashdi'); // Починаємо з Ashdi
+                
+                return scroll.render();
+            };
+
+            this.search = function(source_type) {
+                var _this = this;
+                // ФІКС: замість files.clear() просто очищуємо контейнер вручну
+                files.render().empty(); 
+                Lampa.Select.show({title: 'Пошук на ' + source_type + '...'});
+                
+                var base_url = '';
+                if(source_type === 'ashdi') base_url = 'https://ashdi.vip/api/video?title=';
+                else if(source_type === 'vcdn') base_url = 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
+                else base_url = 'https://videocdn.tv/api/movies?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
+
+                var final_url = api_proxy + encodeURIComponent(base_url + encodeURIComponent(title));
+
+                network.silent(final_url, function(json) {
+                    Lampa.Select.close();
+                    var items = json.data || (Array.isArray(json) ? json : []);
+                    
+                    if (items && items.length > 0) {
+                        items.forEach(function(item) {
+                            var card = Lampa.Template.get('button', {title: item.title || item.name || 'Відео'});
+                            card.on('hover:enter', function() {
+                                var v_url = item.iframe_src || item.url;
+                                if (v_url) {
+                                    if (!v_url.startsWith('http')) v_url = 'https:' + v_url;
+                                    Lampa.Player.play({ url: v_url, title: item.title || title });
+                                }
+                            });
+                            files.append(card);
+                        });
+                    } else {
+                        Lampa.Noty.show('На цьому балансері нічого не знайдено');
+                    }
+                }, function() {
+                    Lampa.Select.close();
+                    Lampa.Noty.show('Помилка проксі-сервера');
+                });
+            };
+
+            this.render = function() { return this.create(); };
+        });
     }
     if (window.Lampa) new KozakTiv().init();
 })();
