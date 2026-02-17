44b68cde466b86f6d2770611d465ffb563b20254
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 10:21:43 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -2,6 +2,9 @@
     'use strict';
 
     function KozakTiv() {
+        // Використовуємо те саме API, що й у професійному плагіні
+        var api_base = 'https://bbe.lme.isroot.in/api/v2';
+
         this.init = function () {
             var _this = this;
             Lampa.Listener.follow('full', function (e) {
@@ -20,7 +23,6 @@
 
         this.render = function (data, html) {
             var _this = this;
-            // Створюємо кнопку в стилі "Козак ТВ"
             var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ (UA)</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
@@ -28,14 +30,16 @@
             container.append(button);
 
             button.on('hover:enter click', function() {
-                // Виклик системного вікна з повною підтримкою балансерів
+                var movie = data.movie;
+                
+                // Штовхаємо Лампу відкрити стандартний онлайн-компонент
                 Lampa.Activity.push({
-                    title: 'Козак ТВ',
+                    title: 'Козак ТВ: ' + (movie.title || movie.name),
                     component: 'online', 
-                    movie: data.movie,
+                    movie: movie,
                     page: 1,
-                    // Використовуємо універсальний шлюз, який обходить блокування
-                    url: 'https://corsproxy.io/?' + encodeURIComponent('https://api.vokino.tv/v2/online')
+                    // Ми використовуємо метод пошуку 'all', який об'єднує Ashdi, VideoCDN та інші
+                    url: api_base + '/search?source=all&tmdb_id=' + (movie.id || '') + '&title=' + encodeURIComponent(movie.title || movie.name)
                 });
             });
         };
