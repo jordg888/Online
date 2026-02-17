ef95471d333b5573673ab58d7d7d99fb9e52d4b8
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 10:41:26 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -7,9 +7,10 @@
             Lampa.Listener.follow('full', function (e) {
                 if (e.type === 'complite') {
                     _this.cleanup();
+                    // Додаємо кнопку через невелику паузу
                     setTimeout(function() {
                         _this.render(e.data, e.object.activity.render());
-                    }, 200);
+                    }, 300);
                 }
             });
         };
@@ -20,54 +21,36 @@
 
         this.render = function (data, html) {
             var _this = this;
-            var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ (UA)</span></div>');
+            // Створюємо кнопку (яскраву, щоб точно побачити)
+            var button = $('<div class="full-start__button selector lampa-kozak-button" style="border: 1px solid #ffde1a;"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ (UA)</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
             if (container.find('.lampa-kozak-button').length) return;
             container.append(button);
 
             button.on('hover:enter click', function() {
-                var movie = data.movie;
-                
-                // Створюємо активність онлайн
-                Lampa.Component.add('kozak_online', Lampa.Component.get('online'));
-                
-                var activity = {
-                    title: 'Козак ТВ',
-                    component: 'kozak_online',
-                    movie: movie,
-                    page: 1
-                };
-
-                // ВАЖЛИВО: ми не даємо прямий URL, а налаштовуємо джерело так, 
-                // як це роблять плагіни, що працюють через bbe.lme.isroot.in
-                activity.onRender = function(object) {
-                    // Якщо в системі є фільтр, додаємо вибір балансерів
-                    if(object.filter) {
-                        object.filter.set('source', [
-                            {title: 'Усі джерела', source: 'all', selected: true},
-                            {title: 'Ashdi (UA)', source: 'ashdi'},
-                            {title: 'VideoCDN', source: 'vcdn'}
-                        ]);
-
-                        object.filter.onSelect = function(item) {
-                            object.search(item.source);
-                        };
-                    }
-                };
-
-                // Формуємо правильний запит до "Бандери"
-                activity.url = 'https://bbe.lme.isroot.in/api/v2/search?source=all&title=' + encodeURIComponent(movie.title || movie.name) + '&tmdb_id=' + (movie.id || '');
-
-                Lampa.Activity.push(activity);
+                // ВАЖЛИВО: Ми викликаємо саме той компонент, який використовує Bandera
+                // В системі Лампи він зазвичай реєструється під назвою 'online' або за посиланням
+                Lampa.Activity.push({
+                    title: 'Козак ТВ: ' + (data.movie.title || data.movie.name),
+                    component: 'online', // Використовуємо стандартний компонент
+                    movie: data.movie,
+                    page: 1,
+                    // Передаємо параметри пошуку через перевірене API
+                    url: 'https://bbe.lme.isroot.in/api/v2/search?source=all&tmdb_id=' + (data.movie.id || '')
+                });
             });
         };
     }
 
-    var timer = setInterval(function(){
-        if(window.Lampa && Lampa.Component && Lampa.Component.get('online')) {
-            clearInterval(timer);
+    // Броньований запуск плагіна
+    function startPlugin() {
+        if (window.Lampa) {
             new KozakTiv().init();
+        } else {
+            setTimeout(startPlugin, 500);
         }
-    }, 500);
+    }
+
+    startPlugin();
 })();
