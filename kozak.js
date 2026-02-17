94512d2d416342efedb7824cba8c0033e47838b0
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 10:50:31 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -9,7 +9,7 @@
                     _this.cleanup();
                     setTimeout(function() {
                         _this.render(e.data, e.object.activity.render());
-                    }, 400);
+                    }, 300);
                 }
             });
         };
@@ -20,42 +20,25 @@
 
         this.render = function (data, html) {
             var _this = this;
-            // Створюємо кнопку Козак ТВ
-            var button = $('<div class="full-start__button selector lampa-kozak-button" style="border: 1px solid #ffde1a !important; background: rgba(255, 222, 26, 0.1) !important;"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle; font-weight: bold; color: #ffde1a;">КОЗАК ТВ</span></div>');
+            var button = $('<div class="full-start__button selector lampa-kozak-button" style="background: #ffde1a !important; color: #000 !important;"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#000"/></svg><span style="vertical-align:middle; font-weight: bold;">ДИВИТИСЬ (КОЗАК ТВ)</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
             if (container.find('.lampa-kozak-button').length) return;
             container.append(button);
 
             button.on('hover:enter click', function() {
-                // ШУКАЄМО РОБОЧИЙ ПЛАГІН
-                // Зазвичай Bandera реєструє себе в системі як 'online' або через спеціальний Listener
-                var found = false;
+                var movie = data.movie;
+                // Викликаємо системний онлайн-компонент
+                Lampa.Component.add('kozak_online', Lampa.Component.get('online'));
                 
-                // Спроба 1: Виклик через стандартний компонент, але з міткою Bandera
-                if (Lampa.Component.get('online')) {
-                    Lampa.Activity.push({
-                        title: 'Козак ТВ',
-                        component: 'online',
-                        movie: data.movie,
-                        page: 1,
-                        source: 'bandera' // Деякі версії розуміють це як сигнал працювати через цей шлюз
-                    });
-                    found = true;
-                }
-
-                // Спроба 2: Якщо перша не спрацювала, імітуємо натискання на кнопку "Онлайн"
-                if (!found) {
-                    var online_btn = $(html).find('.full-start__button').filter(function() {
-                        return $(this).text().toLowerCase().indexOf('онлайн') > -1 || $(this).text().toLowerCase().indexOf('bandera') > -1;
-                    });
-                    
-                    if (online_btn.length) {
-                        online_btn.trigger('click');
-                    } else {
-                        Lampa.Noty.show('Будь ласка, увімкніть плагін Bandera в налаштуваннях');
-                    }
-                }
+                Lampa.Activity.push({
+                    title: 'Козак ТВ',
+                    component: 'kozak_online',
+                    movie: movie,
+                    page: 1,
+                    // Використовуємо стабільний універсальний шлюз
+                    url: 'https://vokino.tv/api/v2/online'
+                });
             });
         };
     }
