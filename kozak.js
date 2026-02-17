8bad19b3f13f3092b06141288c4bf94618f9438c
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 09:27:44 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -2,7 +2,9 @@
     'use strict';
 
     function KozakTiv() {
-        // Спробуємо використати інший підхід до формування посилання
+        // Ми використовуємо перевірений часом публічний проксі для Lampa
+        var proxy = 'https://cors.eu.org/'; 
+
         this.init = function () {
             var _this = this;
             Lampa.Listener.follow('full', function (e) {
@@ -20,7 +22,8 @@
         };
 
         this.render = function (data, html) {
-            var _this = this;
+            var movie = data.movie;
+            var title = movie.title || movie.name;
             var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="white"/></svg><span style="vertical-align:middle;">Козак ТВ</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
@@ -28,23 +31,18 @@
             container.append(button);
 
             button.on('hover:enter click', function() {
-                var movie = data.movie;
-                var title = movie.title || movie.name;
+                // Формуємо запит безпосередньо до балансера через надійний проксі
+                var search_url = proxy + 'https://ashdi.vip/api/video?title=' + encodeURIComponent(title);
 
-                // Використовуємо системний компонент Online
                 Lampa.Activity.push({
                     title: 'Козак ТВ',
                     component: 'online', 
                     movie: movie,
-                    page: 1,
-                    // Ми міняємо структуру запиту, щоб Lampa сама намагалася його "проштовхнути"
-                    url: 'https://vercel-proxy-blue-six.vercel.app/api?url=' + encodeURIComponent('https://ashdi.vip/api/video?title=' + encodeURIComponent(title))
+                    url: search_url
                 });
             });
         };
     }
 
-    if (window.Lampa) {
-        new KozakTiv().init();
-    }
+    if (window.Lampa) new KozakTiv().init();
 })();
