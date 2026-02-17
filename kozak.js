a3de5b22378bf7bb193eb2ce3ee80d4876b9a4aa
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 10:11:32 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -20,22 +20,22 @@
 
         this.render = function (data, html) {
             var _this = this;
-            // Створюємо кнопку, яка виглядає як рідна
-            var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ (Online)</span></div>');
+            // Створюємо кнопку в стилі "Козак ТВ"
+            var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ (UA)</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
             if (container.find('.lampa-kozak-button').length) return;
             container.append(button);
 
             button.on('hover:enter click', function() {
-                // Використовуємо універсальний онлайн-двигун
+                // Виклик системного вікна з повною підтримкою балансерів
                 Lampa.Activity.push({
                     title: 'Козак ТВ',
                     component: 'online', 
                     movie: data.movie,
                     page: 1,
-                    // Це посилання — "золотий стандарт" для роботи з балансерами в Lampa
-                    url: 'https://cors.eu.org/https://api.vokino.tv/v2/online'
+                    // Використовуємо універсальний шлюз, який обходить блокування
+                    url: 'https://corsproxy.io/?' + encodeURIComponent('https://api.vokino.tv/v2/online')
                 });
             });
         };
