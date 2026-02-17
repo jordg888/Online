dc023836b13bae30c4d75d881b2e17f3e5115ca0
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 10:03:23 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -2,9 +2,6 @@
     'use strict';
 
     function KozakTiv() {
-        // Ми використовуємо перевірений часом публічний проксі для Lampa
-        var proxy = 'https://cors.eu.org/'; 
-
         this.init = function () {
             var _this = this;
             Lampa.Listener.follow('full', function (e) {
@@ -22,8 +19,7 @@
         };
 
         this.render = function (data, html) {
-            var movie = data.movie;
-            var title = movie.title || movie.name;
+            var _this = this;
             var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="white"/></svg><span style="vertical-align:middle;">Козак ТВ</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
@@ -31,18 +27,23 @@
             container.append(button);
 
             button.on('hover:enter click', function() {
-                // Формуємо запит безпосередньо до балансера через надійний проксі
-                var search_url = proxy + 'https://ashdi.vip/api/video?title=' + encodeURIComponent(title);
-
+                var movie = data.movie;
+                // Використовуємо універсальний метод виклику онлайн-компонента
+                Lampa.Component.add('kozak_online', Lampa.Storage.get('online_component','online'));
+                
                 Lampa.Activity.push({
                     title: 'Козак ТВ',
-                    component: 'online', 
+                    component: 'kozak_online', 
                     movie: movie,
-                    url: search_url
+                    page: 1,
+                    // Ми підключаємося до потужного двигуна modss, який видасть і балансери, і мови
+                    url: 'https://bwa.to/php/api.php' 
                 });
             });
         };
     }
 
-    if (window.Lampa) new KozakTiv().init();
+    if (window.Lampa) {
+        new KozakTiv().init();
+    }
 })();
