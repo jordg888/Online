31dc4907a668f835130d3899945d32cde671ba4a
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 10:06:54 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -20,24 +20,22 @@
 
         this.render = function (data, html) {
             var _this = this;
-            var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="white"/></svg><span style="vertical-align:middle;">Козак ТВ</span></div>');
+            // Створюємо кнопку, яка виглядає як рідна
+            var button = $('<div class="full-start__button selector lampa-kozak-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ (Online)</span></div>');
             
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
             if (container.find('.lampa-kozak-button').length) return;
             container.append(button);
 
             button.on('hover:enter click', function() {
-                var movie = data.movie;
-                // Використовуємо універсальний метод виклику онлайн-компонента
-                Lampa.Component.add('kozak_online', Lampa.Storage.get('online_component','online'));
-                
+                // Використовуємо універсальний онлайн-двигун
                 Lampa.Activity.push({
                     title: 'Козак ТВ',
-                    component: 'kozak_online', 
-                    movie: movie,
+                    component: 'online', 
+                    movie: data.movie,
                     page: 1,
-                    // Ми підключаємося до потужного двигуна modss, який видасть і балансери, і мови
-                    url: 'https://bwa.to/php/api.php' 
+                    // Це посилання — "золотий стандарт" для роботи з балансерами в Lampa
+                    url: 'https://cors.eu.org/https://api.vokino.tv/v2/online'
                 });
             });
         };
