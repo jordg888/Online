e606f72df5ca152c8f06c234248310545ccb008d
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 11:09:04 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -1,107 +1,94 @@
 (function () {
     'use strict';
 
-    function KozakTiv() {
-        this.init = function () {
-            var _this = this;
-            Lampa.Listener.follow('full', function (e) {
-                if (e.type === 'complite') {
-                    _this.cleanup();
-                    // Додаємо кнопку через таймер, щоб встиг відмалюватися основний інтерфейс
-                    setTimeout(function() {
-                        _this.render(e.data, e.object.activity.render());
-                    }, 300);
-                }
-            });
-        };
+    function startKozak() {
+        // 1. Слухаємо відкриття картки фільму
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type === 'complite') {
+                // Видаляємо стару кнопку, якщо вона є
+                $('.lampa-kozak-btn').remove();
 
-        this.cleanup = function() {
-            $('.lampa-kozak-button').remove();
-        };
+                // 2. Створюємо нову кнопку
+                var button = $('<div class="full-start__button selector lampa-kozak-btn"><span>Козак ТВ</span></div>');
 
-        this.render = function (data, html) {
-            var _this = this;
-            var button = $('<div class="full-start__button selector lampa-kozak-button"><span>Козак ТВ</span></div>');
-            
-            button.on('hover:enter click', function() {
-                _this.open(data.movie);
-            });
+                // 3. Логіка натискання
+                button.on('hover:enter click', function () {
+                    var movie = e.data.movie;
+                    
+                    // Відкриваємо вікно онлайн
+                    Lampa.Activity.push({
+                        title: 'Козак ТВ',
+                        component: 'online',
+                        movie: movie,
+                        page: 1,
+                        onRender: function(object) {
+                            // --- ТУТ МЕХАНІКА ФІЛЬТРІВ ---
+                            // Створюємо кнопку "Джерело" зверху
+                            object.filter.set('source', [
+                                {title: 'Ashdi', source: 'ashdi', selected: true},
+                                {title: 'VideoCDN', source: 'vcdn'}
+                            ]);
 
-            var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
-            if (container.find('.lampa-kozak-button').length) return;
-            container.append(button);
-        };
+                            // Що робити, коли змінили фільтр
+                            object.filter.onSelect = function(item) {
+                                object.search(item.source);
+                            };
 
-        this.open = function (movie) {
-            var _this = this;
-            var current_source = 'ashdi';
+                            // Функція пошуку контенту
+                            object.search = function(source) {
+                                object.loading(true);
+                                var src = source || 'ashdi';
+                                var url = src === 'ashdi' 
+                                    ? 'https://ashdi.vip/api/video?title=' 
+                                    : 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
+                                
+                                // Використовуємо надійний CORS шлюз
+                                var final_url = 'https://corsproxy.io/?' + encodeURIComponent(url + encodeURIComponent(movie.title || movie.name));
 
-            Lampa.Activity.push({
-                title: 'Козак ТВ',
-                component: 'online',
-                movie: movie,
-                page: 1,
-                onRender: function(object) {
-                    // РЕЄСТРУЄМО ФІЛЬТРИ (ВИБІР ДЖЕРЕЛА)
-                    object.filter.set('source', [
-                        {title: 'Ashdi (UA)', source: 'ashdi', selected: true},
-                        {title: 'VideoCDN', source: 'vcdn'}
-                    ]);
-
-                    // ЛОГІКА ПЕРЕМИКАННЯ ФІЛЬТРІВ
-                    object.filter.onSelect = function(item) {
-                        current_source = item.source;
-                        object.search(); 
-                    };
-
-                    // ФУНКЦІЯ ПОШУКУ
-                    object.search = function() {
-                        object.loading(true);
-                        var base_url = current_source === 'ashdi' 
-                            ? 'https://ashdi.vip/api/video?title=' 
-                            : 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
-                        
-                        var query_url = 'https://corsproxy.io/?' + encodeURIComponent(base_url + encodeURIComponent(movie.title || movie.name));
-
-                        $.ajax({
-                            url: query_url,
-                            method: 'GET',
-                            dataType: 'json',
-                            success: function(res) {
-                                var raw = res.data || res;
-                                if (raw && raw.length) {
-                                    var items = raw.map(function(item) {
-                                        return {
-                                            title: item.title || movie.title,
-                                            file: item.file || item.iframe_src || item.url,
-                                            quality: item.quality || 'HD',
-                                            info: current_source.toUpperCase()
-                                        };
-                                    });
-
-                                    object.draw(items, {
-                                        onEnter: function(item) {
-                                            Lampa.Player.play({url: item.file, title: item.title});
+                                $.ajax({
+                                    url: final_url,
+                                    method: 'GET',
+                                    dataType: 'json',
+                                    success: function(res) {
+                                        var data = res.data || res;
+                                        if (data && data.length) {
+                                            var items = data.map(function(i) {
+                                                return {
+                                                    title: i.title || movie.title,
+                                                    file: i.file || i.iframe_src || i.url,
+                                                    quality: i.quality || 'HD'
+                                                };
+                                            });
+                                            object.draw(items, {
+                                                onEnter: function(item) {
+                                                    Lampa.Player.play({url: item.file, title: item.title});
+                                                }
+                                            });
+                                        } else {
+                                            object.empty();
                                         }
-                                    });
-                                } else {
-                                    object.empty();
-                                }
-                                object.loading(false);
-                            },
-                            error: function() {
-                                object.doesNotAnswer();
-                            }
-                        });
-                    };
+                                        object.loading(false);
+                                    },
+                                    error: function() {
+                                        object.doesNotAnswer();
+                                    }
+                                });
+                            };
 
-                    object.search(); // Запуск при відкритті
-                }
-            });
-        };
+                            // Запускаємо перший пошук
+                            object.search();
+                        }
+                    });
+                });
+
+                // 4. Додаємо кнопку в інтерфейс
+                $(e.object.activity.render()).find('.full-start-new__buttons, .full-start__buttons').append(button);
+            }
+        });
     }
 
+    // Запуск плагіна
     if (window.Lampa) {
-        new KozakTiv().init();
+        startKozak();
     }
 })();
