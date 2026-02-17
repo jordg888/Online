0d47fa78b7bf84008a3a19b4c2115777309f4751
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-17 12:23:51 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -1,94 +1,106 @@
 (function () {
     'use strict';
 
-    function startKozak() {
-        // 1. Слухаємо відкриття картки фільму
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type === 'complite') {
-                // Видаляємо стару кнопку, якщо вона є
-                $('.lampa-kozak-btn').remove();
+    function KozakTiv() {
+        var network = new Lampa.Reguest();
 
-                // 2. Створюємо нову кнопку
-                var button = $('<div class="full-start__button selector lampa-kozak-btn"><span>Козак ТВ</span></div>');
+        this.init = function () {
+            var _this = this;
+            Lampa.Listener.follow('full', function (e) {
+                if (e.type === 'complite') {
+                    _this.cleanup();
+                    setTimeout(function() {
+                        _this.render(e.data, e.object.activity.render());
+                    }, 400);
+                }
+            });
+        };
 
-                // 3. Логіка натискання
-                button.on('hover:enter click', function () {
-                    var movie = e.data.movie;
-                    
-                    // Відкриваємо вікно онлайн
-                    Lampa.Activity.push({
-                        title: 'Козак ТВ',
-                        component: 'online',
-                        movie: movie,
-                        page: 1,
-                        onRender: function(object) {
-                            // --- ТУТ МЕХАНІКА ФІЛЬТРІВ ---
-                            // Створюємо кнопку "Джерело" зверху
-                            object.filter.set('source', [
-                                {title: 'Ashdi', source: 'ashdi', selected: true},
-                                {title: 'VideoCDN', source: 'vcdn'}
-                            ]);
+        this.cleanup = function() {
+            $('.lampa-kozak-button').remove();
+        };
 
-                            // Що робити, коли змінили фільтр
-                            object.filter.onSelect = function(item) {
-                                object.search(item.source);
-                            };
+        this.render = function (data, html) {
+            var _this = this;
+            var button = $('<div class="full-start__button selector lampa-kozak-button" style="border: 1px solid #ffde1a !important;"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right:10px;vertical-align:middle;"><path d="M12 2L4.5 20.29L5.21 21L12 18L18.79 21L19.5 20.29L12 2Z" fill="#ffde1a"/></svg><span style="vertical-align:middle;">Козак ТВ</span></div>');
+            
+            var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
+            if (container.find('.lampa-kozak-button').length) return;
+            container.append(button);
 
-                            // Функція пошуку контенту
-                            object.search = function(source) {
-                                object.loading(true);
-                                var src = source || 'ashdi';
-                                var url = src === 'ashdi' 
-                                    ? 'https://ashdi.vip/api/video?title=' 
-                                    : 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=';
-                                
-                                // Використовуємо надійний CORS шлюз
-                                var final_url = 'https://corsproxy.io/?' + encodeURIComponent(url + encodeURIComponent(movie.title || movie.name));
+            button.on('hover:enter click', function() {
+                _this.open(data.movie);
+            });
+        };
 
-                                $.ajax({
-                                    url: final_url,
-                                    method: 'GET',
-                                    dataType: 'json',
-                                    success: function(res) {
-                                        var data = res.data || res;
-                                        if (data && data.length) {
-                                            var items = data.map(function(i) {
-                                                return {
-                                                    title: i.title || movie.title,
-                                                    file: i.file || i.iframe_src || i.url,
-                                                    quality: i.quality || 'HD'
-                                                };
-                                            });
-                                            object.draw(items, {
-                                                onEnter: function(item) {
-                                                    Lampa.Player.play({url: item.file, title: item.title});
-                                                }
-                                            });
-                                        } else {
-                                            object.empty();
-                                        }
-                                        object.loading(false);
-                                    },
-                                    error: function() {
-                                        object.doesNotAnswer();
+        this.open = function (movie) {
+            var _this = this;
+            var current_source = 'ashdi';
+
+            Lampa.Activity.push({
+                title: 'Козак ТВ',
+                component: 'online',
+                movie: movie,
+                page: 1,
+                onRender: function(object) {
+                    // 1. СТВОРЮЄМО ФІЛЬТРИ (Сенс: Користувач може змінити джерело)
+                    object.filter.set('source', [
+                        {title: 'Ashdi (UA)', source: 'ashdi', selected: true},
+                        {title: 'VideoCDN', source: 'vcdn'}
+                    ]);
+
+                    // 2. ОБРОБКА ВИБОРУ (Сенс: Перехоплюємо клік на фільтр)
+                    object.filter.onSelect = function(item) {
+                        current_source = item.source;
+                        object.search(); 
+                    };
+
+                    // 3. ПОШУК (Сенс: Йдемо за даними)
+                    object.search = function() {
+                        object.loading(true);
+                        
+                        // Використовуємо шлях, який перевіряв твій скрипт check.sh
+                        var api_url = current_source === 'ashdi' 
+                            ? 'https://ashdi.vip/api/video?title=' + encodeURIComponent(movie.title || movie.name)
+                            : 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=' + encodeURIComponent(movie.title || movie.name);
+
+                        // Проксі для обходу блокувань
+                        var final_url = 'https://corsproxy.io/?' + encodeURIComponent(api_url);
+
+                        network.silent(final_url, function(res) {
+                            var raw = res.data || res;
+                            if (raw && raw.length) {
+                                var items = raw.map(function(item) {
+                                    return {
+                                        title: item.title || movie.title,
+                                        file: item.file || item.iframe_src || item.url,
+                                        quality: item.quality || 'HD',
+                                        info: current_source.toUpperCase()
+                                    };
+                                });
+
+                                // Малюємо контент
+                                object.draw(items, {
+                                    onEnter: function(i) {
+                                        Lampa.Player.play({url: i.file, title: i.title});
                                     }
                                 });
-                            };
+                            } else {
+                                object.empty();
+                            }
+                            object.loading(false);
+                        }, function() {
+                            object.doesNotAnswer();
+                        });
+                    };
 
-                            // Запускаємо перший пошук
-                            object.search();
-                        }
-                    });
-                });
-
-                // 4. Додаємо кнопку в інтерфейс
-                $(e.object.activity.render()).find('.full-start-new__buttons, .full-start__buttons').append(button);
-            }
-        });
+                    object.search();
+                }
+            });
+        };
     }
 
-    // Запуск плагіна
     if (window.Lampa) {
-        startKozak();
+        new KozakTiv().init();
     }
 })();
