1af75d8d11c4d737028f371ca18f7e8c73d90744
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 15:31:23 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,64 +1,30 @@
 (function(){
-    var plugin = {
-        title: 'Балансери',
-        id: 'balancers_plugin',
+    Lampa.Listener.follow('full', function(e){
+        if(e.type === 'build' && e.card){
+            // створюємо кнопку
+            var btn = $('<div class="selector">Балансери</div>');
+            
+            // додаємо в блок кнопок картки
+            e.card.find('.full-start').append(btn);
 
-        init: function(){
-            // Реєструємо плагін у системі
-            Lampa.Component.add('balancers_plugin', this.component, this);
-
-            // Хук на картку відео
-            Lampa.Listener.follow('full', function(e){
-                if(e.type === 'build' && e.card){
-                    // додаємо кнопку в картку
-                    var btn = $('<div class="balancer_button selector">Балансери</div>');
-                    btn.on('hover:enter', function(){
-                        plugin.showMenu(e.card);
-                    });
-                    e.card.find('.full-start').append(btn);
-                }
-            });
-        },
-
-        // Меню вибору балансера
-        showMenu: function(card){
-            var balanceMenu = Lampa.Select.create({
-                title: 'Виберіть балансер',
-                items: [
-                    {title: 'Балансер 1', url: 'https://balancer1/api/video?id='+card.data.id},
-                    {title: 'Балансер 2', url: 'https://balancer2/api/video?id='+card.data.id}
-                ],
-                onSelect: function(item){
-                    plugin.loadVideos(item.url);
-                }
-            });
-
-            Lampa.Select.open(balanceMenu);
-        },
-
-        // Завантаження відео
-        loadVideos: function(url){
-            var network = new Lampa.Reguest();
-            network.silent(url, function(data){
-                var videoMenu = Lampa.Select.create({
-                    title: 'Виберіть відео',
-                    items: data.results.map(function(video){
-                        return {title: video.name, url: video.stream};
-                    }),
-                    onSelect: function(video){
+            // реакція на натискання
+            btn.on('hover:enter', function(){
+                var menu = Lampa.Select.create({
+                    title: 'Виберіть балансер',
+                    items: [
+                        {title: 'Балансер 1', url: 'https://balancer1/video.m3u8'},
+                        {title: 'Балансер 2', url: 'https://balancer2/video.m3u8'}
+                    ],
+                    onSelect: function(item){
                         Lampa.Player.play({
-                            title: video.title,
-                            url: video.url
+                            title: item.title,
+                            url: item.url
                         });
                     }
                 });
 
-                Lampa.Select.open(videoMenu);
+                Lampa.Select.open(menu);
             });
-        },
-
-        component: function(){}
-    };
-
-    plugin.init();
+        }
+    });
 })();
