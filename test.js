be719219fc77df1ebe9a23f2dc7f04901781e3f2
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 15:39:19 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,29 +1,27 @@
 (function(){
     Lampa.Listener.follow('full', function(e){
         if(e.type === 'build' && e.card){
-            // створюємо кнопку
-            var btn = $('<div class="selector">Балансери</div>');
+            // додаємо системну кнопку
+            e.card.addAction({
+                name: 'Балансери',
+                icon: 'menu', // системна іконка
+                onSelect: function(){
+                    var menu = Lampa.Select.create({
+                        title: 'Виберіть балансер',
+                        items: [
+                            {title: 'Балансер 1', url: 'https://balancer1/video.m3u8'},
+                            {title: 'Балансер 2', url: 'https://balancer2/video.m3u8'}
+                        ],
+                        onSelect: function(item){
+                            Lampa.Player.play({
+                                title: item.title,
+                                url: item.url
+                            });
+                        }
+                    });
 
-            // додаємо у блок кнопок картки
-            e.card.find('.full-start').append(btn);
-
-            // реакція на натискання
-            btn.on('hover:enter', function(){
-                var menu = Lampa.Select.create({
-                    title: 'Виберіть балансер',
-                    items: [
-                        {title: 'Балансер 1', url: 'https://balancer1/video.m3u8'},
-                        {title: 'Балансер 2', url: 'https://balancer2/video.m3u8'}
-                    ],
-                    onSelect: function(item){
-                        Lampa.Player.play({
-                            title: item.title,
-                            url: item.url
-                        });
-                    }
-                });
-
-                Lampa.Select.open(menu);
+                    Lampa.Select.open(menu);
+                }
             });
         }
     });
