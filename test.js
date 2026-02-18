defe99412d776f010841da22c95e0835de690606
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 23:26:23 +0200

    Update test.js

--- test.js
+++ test.js
@@ -15,72 +15,48 @@
         title: PLUGIN_NAME,
         create: function() {
             return function(component, object) {
-                var network = new Lampa.Reguest();
                 var movie = object.movie || {};
 
                 // Пошук контенту
                 this.search = function() {
                     var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
                     
-                    if (movie.year) url += '&year=' + movie.year;
-                    if (movie.imdb_id) url += '&imdb_id=' + movie.imdb_id;
-
-                    network.silent(url, function(json) {
-                        if (json && json.results && json.results.length) {
-                            var items = [];
-                            for (var i = 0; i < json.results.length; i++) {
-                                var item = json.results[i];
-                                items.push({
-                                    id: item.id,
-                                    title: item.name,
-                                    info: 'Балансер',
-                                    year: movie.year,
-                                    data: item
-                                });
-                            }
-                            component.draw(items, {
-                                onEnter: function(item) {
-                                    getVideo(item.id, movie);
-                                }
+                    // ТЕСТОВІ ДАНІ (якщо API не працює)
+                    var testData = {
+                        results: [
+                            {id: 'uaflix', name: 'Uaflix'},
+                            {id: 'animeon', name: 'AnimeON'},
+                            {id: 'bamboo', name: 'Bamboo'},
+                            {id: 'mikai', name: 'Mikai'}
+                        ]
+                    };
+                    
+                    // Показуємо тестові дані
+                    setTimeout(function() {
+                        var items = [];
+                        for (var i = 0; i < testData.results.length; i++) {
+                            var item = testData.results[i];
+                            items.push({
+                                title: item.name,
+                                info: 'Балансер',
+                                data: item
                             });
-                        } else {
-                            component.empty();
                         }
-                    }, function() {
-                        component.doesNotAnswer();
-                    });
+                        
+                        component.draw(items, {
+                            onEnter: function(item) {
+                                alert('Вибрано: ' + item.title);
+                            }
+                        });
+                    }, 500);
                 };
 
-                // Отримання відео
-                function getVideo(balancerId, movieData) {
-                    var url = API_URL + '/video?balancer=' + balancerId +
-                             '&title=' + encodeURIComponent(movieData.title || '');
-                    
-                    if (movieData.year) url += '&year=' + movieData.year;
-
-                    network.silent(url, function(json) {
-                        if (json && json.url) {
-                            Lampa.Player.play({
-                                url: json.url,
-                                title: movieData.title,
-                                quality: json.qualitys || {},
-                                subtitles: json.subtitles || []
-                            });
-                        } else {
-                            component.pushError('Не вдалося отримати відео');
-                        }
-                    });
-                }
-
-                // Скидання
                 this.reset = function() {
                     component.reset();
-                    network.clear();
                 };
 
-                // Знищення
                 this.destroy = function() {
-                    network.clear();
+                    // Нічого не робимо
                 };
             };
         }
