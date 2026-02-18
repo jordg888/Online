dd0660b133d7768be1a2391347567b8fd107bbef
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 23:31:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,66 +1,84 @@
 (function() {
     'use strict';
 
-    // ========== НАЛАШТУВАННЯ ==========
     var API_URL = 'https://api-plug-lime.vercel.app/api';
     var PLUGIN_NAME = 'My Balancer';
     var PLUGIN_KEY = 'my_balancer';
-    // =================================
 
     console.log(PLUGIN_NAME + ': завантаження...');
 
-    // Створюємо провайдер
-    Lampa.Provider.add({
-        name: PLUGIN_KEY,
-        title: PLUGIN_NAME,
-        create: function() {
-            return function(component, object) {
-                var movie = object.movie || {};
+    // ==== КОПІЮЄМО СТРУКТУРУ З BANDERA ====
+    function createV2() {
+        return function(component, object) {
+            var network = new Lampa.Reguest();
+            var movie = object.movie || {};
+            
+            console.log('Пошук для:', movie.title);
 
-                // Пошук контенту
-                this.search = function() {
-                    var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
-                    
-                    // ТЕСТОВІ ДАНІ (якщо API не працює)
-                    var testData = {
-                        results: [
-                            {id: 'uaflix', name: 'Uaflix'},
-                            {id: 'animeon', name: 'AnimeON'},
-                            {id: 'bamboo', name: 'Bamboo'},
-                            {id: 'mikai', name: 'Mikai'}
-                        ]
-                    };
-                    
-                    // Показуємо тестові дані
-                    setTimeout(function() {
+            // Пошук балансерів
+            this.search = function() {
+                var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
+                
+                network.silent(url, function(json) {
+                    if (json && json.results) {
                         var items = [];
-                        for (var i = 0; i < testData.results.length; i++) {
-                            var item = testData.results[i];
+                        for (var i = 0; i < json.results.length; i++) {
                             items.push({
-                                title: item.name,
+                                title: json.results[i].name,
                                 info: 'Балансер',
-                                data: item
+                                data: json.results[i]
                             });
                         }
-                        
                         component.draw(items, {
                             onEnter: function(item) {
-                                alert('Вибрано: ' + item.title);
+                                getVideo(item.data.id, movie);
                             }
                         });
-                    }, 500);
-                };
-
-                this.reset = function() {
-                    component.reset();
-                };
-
-                this.destroy = function() {
-                    // Нічого не робимо
-                };
+                    } else {
+                        component.empty();
+                    }
+                }, function() {
+                    component.doesNotAnswer();
+                });
             };
-        }
-    });
 
-    console.log('✅ Плагін-джерело зареєстровано');
+            // Отримання відео
+            function getVideo(id, movieData) {
+                var url = API_URL + '/video?balancer=' + id + '&title=' + encodeURIComponent(movieData.title || '');
+                
+                network.silent(url, function(json) {
+                    if (json && json.url) {
+                        Lampa.Player.play({
+                            url: json.url,
+                            title: movieData.title,
+                            quality: json.qualitys || {}
+                        });
+                    } else {
+                        component.pushError('Немає відео');
+                    }
+                });
+            }
+
+            // Обов'язкові функції
+            this.reset = function() {
+                component.reset();
+                network.clear();
+            };
+
+            this.destroy = function() {
+                network.clear();
+            };
+        };
+    }
+
+    // Реєструємо плагін
+    if (window.Lampa && Lampa.Provider) {
+        Lampa.Provider.add({
+            name: PLUGIN_KEY,
+            title: PLUGIN_NAME,
+            create: createV2()
+        });
+        console.log('✅ Плагін зареєстровано');
+    }
+
 })();
