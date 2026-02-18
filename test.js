abf33363c2786d5bbf59be0f05fb24d47f2eb23e
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 23:45:18 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,55 +1,53 @@
 (function() {
     'use strict';
 
-    // ========== ВАШІ ДАНІ ==========
     var API_URL = 'https://api-plug-lime.vercel.app/api';
     var PLUGIN_NAME = 'My Balancer';
     var PLUGIN_KEY = 'my_balancer';
-    // ===============================
 
     console.log(PLUGIN_NAME + ': завантаження...');
 
     // ТОЧНА КОПІЯ СТРУКТУРИ BANDERA
+    function createPlugin() {
+        return function(component, object) {
+            var network = new Lampa.Reguest();
+            var movie = object.movie || {};
+            
+            // ПОШУК
+            this.search = function() {
+                var items = [
+                    { title: 'Uaflix', info: 'Балансер 1' },
+                    { title: 'AnimeON', info: 'Балансер 2' },
+                    { title: 'Bamboo', info: 'Балансер 3' }
+                ];
+                
+                component.draw(items, {
+                    onEnter: function(item) {
+                        Lampa.Noty.show('Вибрано: ' + item.title);
+                        setTimeout(Lampa.Noty.hide, 2000);
+                    }
+                });
+            };
+
+            // ОБОВ'ЯЗКОВІ МЕТОДИ
+            this.reset = function() {
+                component.reset();
+                network.clear();
+            };
+
+            this.destroy = function() {
+                network.clear();
+            };
+        };
+    }
+
+    // РЕЄСТРАЦІЯ
     Lampa.Provider.add({
         name: PLUGIN_KEY,
         title: PLUGIN_NAME,
-        create: function() {
-            return function(component, object) {
-                var network = new Lampa.Reguest();
-                var movie = object.movie || {};
-
-                // ПОШУК - як у Bandera
-                this.search = function() {
-                    var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
-                    
-                    // Тестові дані, якщо API не працює
-                    var items = [
-                        { title: 'Uaflix', info: 'Балансер' },
-                        { title: 'AnimeON', info: 'Балансер' },
-                        { title: 'Bamboo', info: 'Балансер' },
-                        { title: 'Mikai', info: 'Балансер' }
-                    ];
-                    
-                    component.draw(items, {
-                        onEnter: function(item) {
-                            Lampa.Noty.show('Вибрано: ' + item.title);
-                            setTimeout(Lampa.Noty.hide, 2000);
-                        }
-                    });
-                };
-
-                // ОБОВ'ЯЗКОВІ МЕТОДИ
-                this.reset = function() {
-                    component.reset();
-                    network.clear();
-                };
-
-                this.destroy = function() {
-                    network.clear();
-                };
-            };
-        }
+        create: createPlugin()
     });
 
     console.log('✅ Плагін зареєстровано');
+
 })();
