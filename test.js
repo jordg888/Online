2bec48677b39c73632d599ee3d8eaed368e285b6
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 23:08:14 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,12 +2,17 @@
     'use strict';
     
     setTimeout(function() {
-        // Шукаємо всі кнопки і фарбуємо їх в червоний
-        $('div').each(function() {
-            if ($(this).text().indexOf('Онлайн') >= 0) {
-                $(this).css('border', '5px solid red');
-                $(this).css('background', 'yellow');
+        // Шукаємо всі символи з кнопками
+        var symbols = document.getElementsByTagName('symbol');
+        
+        for(var i = 0; i < symbols.length; i++) {
+            console.log('Symbol ' + i + ': ' + symbols[i].id);
+            
+            // Якщо знайшли символ feed - аналізуємо його
+            if(symbols[i].id == 'sprite-feed') {
+                var content = symbols[i].innerHTML;
+                console.log('Feed content:', content);
             }
-        });
+        }
     }, 5000);
 })();
