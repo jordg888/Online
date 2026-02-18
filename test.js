9c012cf4fe1c86a43a215440ac5ac60e69793932
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 23:35:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,80 +5,70 @@
     var PLUGIN_NAME = 'My Balancer';
     var PLUGIN_KEY = 'my_balancer';
 
-    console.log(PLUGIN_NAME + ': завантаження...');
-
-    // ==== КОПІЮЄМО СТРУКТУРУ З BANDERA ====
-    function createV2() {
-        return function(component, object) {
-            var network = new Lampa.Reguest();
-            var movie = object.movie || {};
-            
-            console.log('Пошук для:', movie.title);
-
-            // Пошук балансерів
-            this.search = function() {
-                var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
+    // Копіюємо структуру з Bandera
+    Lampa.Provider.add({
+        name: PLUGIN_KEY,
+        title: PLUGIN_NAME,
+        create: function() {
+            return function(component, object) {
+                var network = new Lampa.Reguest();
+                var movie = object.movie || {};
                 
-                network.silent(url, function(json) {
-                    if (json && json.results) {
-                        var items = [];
-                        for (var i = 0; i < json.results.length; i++) {
-                            items.push({
-                                title: json.results[i].name,
-                                info: 'Балансер',
-                                data: json.results[i]
+                // Основний пошук
+                this.search = function() {
+                    var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
+                    
+                    network.silent(url, function(json) {
+                        if (json && json.results) {
+                            var items = [];
+                            for (var i = 0; i < json.results.length; i++) {
+                                items.push({
+                                    title: json.results[i].name,
+                                    info: 'Балансер'
+                                });
+                            }
+                            component.draw(items, {
+                                onEnter: function(item) {
+                                    Lampa.Noty.show('Вибрано: ' + item.title);
+                                }
                             });
                         }
-                        component.draw(items, {
-                            onEnter: function(item) {
-                                getVideo(item.data.id, movie);
-                            }
-                        });
-                    } else {
-                        component.empty();
-                    }
-                }, function() {
-                    component.doesNotAnswer();
-                });
-            };
+                    });
+                };
 
-            // Отримання відео
-            function getVideo(id, movieData) {
-                var url = API_URL + '/video?balancer=' + id + '&title=' + encodeURIComponent(movieData.title || '');
-                
-                network.silent(url, function(json) {
-                    if (json && json.url) {
-                        Lampa.Player.play({
-                            url: json.url,
-                            title: movieData.title,
-                            quality: json.qualitys || {}
+                // Обов'язкові методи
+                this.reset = function() {
+                    component.reset();
+                };
+
+                this.destroy = function() {
+                    network.clear();
+                };
+            };
+        }
+    });
+
+    // Додаємо кнопку в картку (якщо це можливо)
+    setTimeout(function() {
+        setInterval(function() {
+            var container = $('.full-start__buttons, .full-start-new__buttons').first();
+            if (container.length && !$('.my-balancer-source-btn').length) {
+                var btn = $('<div class="full-start__button selector">' +
+                           '<div>⚖️</div>' +
+                           '<span>Балансер</span>' +
+                           '</div>');
+                container.append(btn);
+                btn.on('click', function() {
+                    var page = Lampa.Page.current();
+                    if (page && page.data) {
+                        Lampa.Page.open('source', {
+                            source: PLUGIN_KEY,
+                            movie: page.data.movie || page.data
                         });
-                    } else {
-                        component.pushError('Немає відео');
                     }
                 });
             }
-
-            // Обов'язкові функції
-            this.reset = function() {
-                component.reset();
-                network.clear();
-            };
-
-            this.destroy = function() {
-                network.clear();
-            };
-        };
-    }
-
-    // Реєструємо плагін
-    if (window.Lampa && Lampa.Provider) {
-        Lampa.Provider.add({
-            name: PLUGIN_KEY,
-            title: PLUGIN_NAME,
-            create: createV2()
-        });
-        console.log('✅ Плагін зареєстровано');
-    }
+        }, 2000);
+    }, 3000);
 
 })();
