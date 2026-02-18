b5c4d24b92df806bbfa53c756dff8c6ad91e53f4
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-18 10:15:17 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,83 +1,44 @@
 (function() {
     'use strict';
 
-    var API_URL = 'https://api-plug-lime.vercel.app/api';
-    var PLUGIN_NAME = 'My Balancer';
     var PLUGIN_KEY = 'my_balancer';
+    var PLUGIN_NAME = 'My Balancer';
 
-    console.log(PLUGIN_NAME + ': завантаження...');
-
-    // ==================== ДЖЕРЕЛО (як у Bandera) ====================
+    // ==================== ТІЛЬКИ ДЖЕРЕЛО ====================
     if (window.Lampa && Lampa.Provider) {
         Lampa.Provider.add({
             name: PLUGIN_KEY,
             title: PLUGIN_NAME,
             create: function() {
                 return function(component, object) {
-                    var network = new Lampa.Reguest();
-                    var movie = object.movie || {};
-
                     this.search = function() {
-                        var url = API_URL + '/search?title=' + encodeURIComponent(movie.title || '');
-                        
-                        network.silent(url, function(json) {
-                            if (json && json.results) {
-                                component.draw(json.results.map(function(item) {
-                                    return {
-                                        title: item.name,
-                                        info: 'Балансер',
-                                        data: item
-                                    };
-                                }), {
-                                    onEnter: function(item) {
-                                        getVideo(item.data.id, movie);
-                                    }
-                                });
-                            } else {
-                                component.empty();
+                        component.draw([
+                            {title: 'Uaflix', info: 'Балансер'},
+                            {title: 'AnimeON', info: 'Балансер'},
+                            {title: 'Bamboo', info: 'Балансер'}
+                        ], {
+                            onEnter: function(item) {
+                                alert('Вибрано: ' + item.title);
                             }
-                        }, function() {
-                            component.doesNotAnswer();
                         });
                     };
-
-                    function getVideo(balancerId, movie) {
-                        var url = API_URL + '/video?balancer=' + balancerId + 
-                                 '&title=' + encodeURIComponent(movie.title || '');
-                        
-                        network.silent(url, function(json) {
-                            if (json && json.url) {
-                                Lampa.Player.play({
-                                    url: json.url,
-                                    title: movie.title
-                                });
-                            }
-                        });
-                    }
                 };
             }
         });
     }
 
-    // ==================== КНОПКА В КАРТЦІ ====================
+    // ==================== ТІЛЬКИ КНОПКА ====================
     setTimeout(function() {
-        // Додаємо кнопку в картку
-        function addButton() {
-            var container = $('.full-start__buttons, .full-start-new__buttons').first();
-            
+        setInterval(function() {
+            var container = $('.full-start__buttons').first();
             if (container.length && !$('.my-source-btn').length) {
                 var button = $('<div class="full-start__button selector my-source-btn">' +
-                               '<div style="font-size: 24px;">⚖️</div>' +
-                               '<span>Балансер</span>' +
+                               '<div>⚖️</div>' +
+                               '<span>Balancer</span>' +
                                '</div>');
-                
                 container.append(button);
                 
-                // Відкриваємо джерело при натисканні
-                button.on('hover:enter click', function(e) {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    
+                button.on('click', function() {
                     var page = Lampa.Page.current();
                     if (page && page.data) {
                         Lampa.Page.open('source', {
@@ -86,13 +47,6 @@
                         });
                     }
                 });
-            }
-        }
-
-        // Перевіряємо кожну секунду
-        setInterval(function() {
-            if (Lampa.Page.current() && Lampa.Page.current().name === 'card') {
-                addButton();
             }
         }, 1000);
     }, 2000);
