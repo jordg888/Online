d57e1589285a10d33e4ffc2207b227cafffdf362
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 01:24:12 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
 
+    var hostkey = 'golampaua.mooo.com';
+
     function KozakTiv() {
         var _this = this;
 
@@ -14,61 +16,60 @@
 
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
-            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #ffde1a !important; color: #000 !important;"><span>КОЗАК ТВ</span></div>');
+            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #ffde1a !important; color: #000 !important; border-radius: 5px;"><span>КОЗАК ТВ</span></div>');
             btn.on('hover:enter click', function () { _this.search(data.movie); });
             $(html).find('.full-start-new__buttons, .full-start__buttons').append(btn);
         };
 
         this.search = function (movie) {
             var title = movie.title || movie.name;
-            // Використовуємо спеціалізований шлюз для обходу гео-блокувань
-            var base_url = 'https://videocdn.tv/api/short?api_token=3i40v5i7z6CcU4SHe627S74y704mIu62&title=' + encodeURIComponent(title);
-            var proxy_url = 'https://api.allorigins.win/get?url=' + encodeURIComponent(base_url);
-
+            
+            // Показуємо вікно очікування
             Lampa.Select.show({
                 title: 'Пошук: ' + title,
-                items: [{title: 'Обхід блокування (EU Proxy)...', any: true}],
+                items: [{title: 'Встановлення захищеного з’єднання...', any: true}],
                 onSelect: function() {},
                 onBack: function() { Lampa.Controller.toggle('content'); }
             });
 
+            // Використовуємо шлях з твого робочого плагіна
+            var url = 'http://' + hostkey + '/lite/online?id=' + movie.id + 
+                      '&title=' + encodeURIComponent(title) + 
+                      '&original_title=' + encodeURIComponent(movie.original_title || '') +
+                      '&year=' + (movie.release_date || '').slice(0,4);
+
             var network = new Lampa.Reguest();
-            network.silent(proxy_url, function (res) {
-                // allorigins повертає дані у полі contents як рядок
-                try {
-                    var response = JSON.parse(res.contents);
-                    var data = response.data || response;
+            
+            // Спеціальний заголовок для імітації Lampac-клієнта
+            var headers = {
+                'X-Kit-AesGcm': Lampa.Storage.get('aesgcmkey', '')
+            };
 
-                    if (data && data.length) {
-                        var items = data.map(function (i) {
-                            return {
-                                title: i.title || title,
-                                subtitle: 'Джерело: VideoCDN',
-                                file: i.iframe_src || i.file
-                            };
-                        });
+            network.silent(url, function (res) {
+                if (res && res.length) {
+                    var items = res.map(function (i) {
+                        return {
+                            title: i.name || i.title || title,
+                            subtitle: i.quality || 'HD (UA/RU)',
+                            file: i.url || i.video || i.file
+                        };
+                    });
 
-                        Lampa.Select.show({
-                            title: 'Знайдено через шлюз',
-                            items: items,
-                            onSelect: function (item) {
-                                var video = item.file;
-                                if (video.indexOf('//') === 0) video = 'https:' + video;
-                                Lampa.Player.play({ url: video, title: item.title });
-                            }
-                        });
-                    } else {
-                        Lampa.Noty.show('Фільм не знайдено в базі');
-                        Lampa.Select.close();
-                    }
-                } catch (e) {
-                    Lampa.Noty.show('Помилка дешифрування даних');
+                    Lampa.Select.show({
+                        title: 'Знайдено в мережі Козак ТВ',
+                        items: items,
+                        onSelect: function (item) {
+                            Lampa.Player.play({ url: item.file, title: item.title });
+                        }
+                    });
+                } else {
+                    Lampa.Noty.show('На жаль, у захищених базах нічого не знайдено');
                     Lampa.Select.close();
                 }
             }, function () {
-                Lampa.Noty.show('Сервер у Польщі/Європі не відповідає');
+                Lampa.Noty.show('Помилка тунелювання. Спробуйте ввімкнути Proxy в налаштуваннях.');
                 Lampa.Select.close();
-            });
+            }, false, {headers: headers});
         };
     }
 
