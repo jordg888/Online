f831a2bee55aacca58469e86bcfe4cf8a4bfafbf
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 11:35:40 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -1,58 +1,58 @@
 (function () {
     'use strict';
 
-    function KozakTiv() {
+    function KozakPlugin() {
         var _this = this;
 
         this.init = function () {
+            // Підписуємось на відкриття картки фільму
             Lampa.Listener.follow('full', function (e) {
                 if (e.type === 'complite' || e.type === 'ready') {
-                    _this.render(e.data, e.object.activity.render());
+                    _this.addButton(e.data, e.object.activity.render());
                 }
             });
         };
 
-        this.render = function (data, html) {
-            $('.lampa-kozak-btn').remove();
-            // Яскраво-жовта кнопка для привернення уваги
-            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #f1c40f !important; color: #000 !important; border-radius: 8px; margin-top: 10px; display: flex; align-items: center; justify-content: center; height: 3.5em; cursor: pointer; font-weight: bold;"><span>КОЗАК ТВ: FORCE START</span></div>');
-            
+        this.addButton = function (data, html) {
+            // Очищуємо стару кнопку, якщо вона була
+            $(html).find('.kozak-button').remove();
+
+            // Створюємо стандартну кнопку Lampa
+            var btn = $('<div class="full-start__button selector kozak-button"><span>КОЗАК ТВ</span></div>');
+
             btn.on('click', function () {
-                _this.search(data.movie);
+                _this.openPlayer(data.movie);
             });
 
-            var container = $(html).find('.full-start-new__buttons, .full-start__buttons, .full-start__btns');
-            if (container.length > 0) container.append(btn);
-            else $(html).find('.full-start__description').after(btn);
+            // Шукаємо контейнер для кнопок (сумісність з різними версіями)
+            var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
+            if (container.length) {
+                container.append(btn);
+            } else {
+                $(html).find('.full-start__description').after(btn);
+            }
         };
 
-        this.search = function (movie) {
-            var title = movie.title || movie.name;
-            // Використовуємо пряме посилання на плеєр, яке зазвичай обходить SES
+        this.openPlayer = function (movie) {
+            // Формуємо URL плеєра (TMDB ID)
             var video_url = 'https://vjs.su/embed/tmdb/' + movie.id;
 
-            Lampa.Noty.show('Обхід захисту SES...');
-
-            // Замість компонента iframe, створюємо пряму команду на відкриття
-            var activity = Lampa.Activity.active();
-            
+            // Використовуємо вбудований метод Lampa для відкриття iframe
+            // Це найнадійніший спосіб, який підтримується всіма версіями
             Lampa.Component.add('iframe', {
-                title: 'Козак ТВ: ' + title,
+                title: 'Козак ТВ: ' + (movie.title || movie.name),
                 url: video_url,
                 clean: true,
-                onBack: function(){
+                onBack: function() {
                     Lampa.Activity.backward();
                 }
             });
-            
-            // Додатково пробуємо "проштовхнути" фокус, якщо SES його блокує
-            setTimeout(function(){
-                Lampa.Controller.toggle('content');
-            }, 500);
         };
     }
 
+    // Запуск плагіна
     if (window.Lampa) {
-        new KozakTiv().init();
+        var kozak = new KozakPlugin();
+        kozak.init();
     }
 })();
