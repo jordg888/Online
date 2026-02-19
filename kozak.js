fbe1dd3f489ba0a06ae5bf37ba1382397c3d3c42
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 11:07:06 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -5,7 +5,6 @@
         var _this = this;
 
         this.init = function () {
-            // Слухаємо відкриття картки фільму
             Lampa.Listener.follow('full', function (e) {
                 if (e.type === 'complite' || e.type === 'ready') {
                     _this.render(e.data, e.object.activity.render());
@@ -15,61 +14,62 @@
 
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
-            
-            // Створюємо кнопку в стилі Лампи
             var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #27ae60 !important; color: #fff !important; border-radius: 5px; margin-top: 10px;"><span>КОЗАК ТВ</span></div>');
             
             btn.on('click', function () {
-                console.log('Kozak:', 'Пошук відео для:', data.movie.title || data.movie.name);
                 _this.search(data.movie);
             });
 
-            // Додаємо кнопку до загального блоку
             var container = $(html).find('.full-start-new__buttons, .full-start__buttons');
-            if (container.length > 0) {
-                container.append(btn);
-            } else {
-                $(html).append(btn);
-            }
+            if (container.length > 0) container.append(btn);
+            else $(html).append(btn);
         };
 
         this.search = function (movie) {
             var title = movie.title || movie.name;
-            Lampa.Noty.show('Шукаю на серверах...');
+            Lampa.Noty.show('Шукаю через ' + title + '...');
 
-            // Використовуємо Alloha API через native-запит
-            // Він автоматично пройде через твій робочий сокет kurwa-bober
             var api_url = 'https://api.alloha.tv/?token=044417740f9350436d7a71888e5d61&name=' + encodeURIComponent(title);
 
-            Lampa.Reguest.native(api_url, function(res) {
-                console.log('Kozak: Відповідь отримана', res);
-                
-                var video_url = "";
-                if (res.data) {
-                    video_url = res.data.iframe_url || res.data.iframe;
-                }
+            // Шукаємо робочий метод запиту в надрах Лампи
+            // Спробуємо по черзі всі відомі варіанти
+            var network = Lampa.Network || Lampa.Reguest || Lampa.Request;
+            var sendNative = network ? (network.native || network.get) : null;
 
-                if (video_url) {
-                    // Якщо посилання починається з //, додаємо https:
-                    if (video_url.indexOf('//') === 0) video_url = 'https:' + video_url;
-                    
-                    Lampa.Noty.show('Запускаю плеєр...');
-                    Lampa.Player.play({
-                        url: video_url,
-                        title: title
-                    });
-                } else {
-                    Lampa.Noty.show('Відео не знайдено');
-                }
-            }, function(err) {
-                console.error('Kozak Error:', err);
-                Lampa.Noty.show('Помилка запиту');
-            });
+            if (typeof sendNative === 'function') {
+                sendNative(api_url, function(res) {
+                    console.log('Kozak: Знайдено!', res);
+                    var video_url = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
+
+                    if (video_url) {
+                        if (video_url.indexOf('//') === 0) video_url = 'https:' + video_url;
+                        Lampa.Player.play({ url: video_url, title: title });
+                    } else {
+                        Lampa.Noty.show('Відео не знайдено');
+                    }
+                }, function() {
+                    Lampa.Noty.show('Помилка мережі (API)');
+                });
+            } else {
+                // Якщо Лампа забарикадувала всі двері, спробуємо старий добрий jQuery (але може бути CORS)
+                console.log('Kozak: Використовую запасний метод (jQuery)');
+                $.ajax({
+                    url: api_url,
+                    dataType: 'json',
+                    success: function(res) {
+                        var video_url = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
+                        if (video_url) {
+                            if (video_url.indexOf('//') === 0) video_url = 'https:' + video_url;
+                            Lampa.Player.play({ url: video_url, title: title });
+                        }
+                    },
+                    error: function() {
+                        Lampa.Noty.show('Не вдалося виконати запит');
+                    }
+                });
+            }
         };
     }
 
-    // Запуск плагіна
-    if (window.Lampa) {
-        new KozakTiv().init();
-    }
+    if (window.Lampa) new KozakTiv().init();
 })();
