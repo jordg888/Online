b594c68c46e374cb3122649aec4e09de82236084
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 03:04:10 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -15,54 +15,51 @@
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
             var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #ffde1a !important; color: #000 !important; border: 2px solid #fff;"><span>КОЗАК ТВ</span></div>');
-            btn.on('hover:enter click', function () { _this.openPage(data.movie); });
+            btn.on('hover:enter click', function () { _this.open(data.movie); });
             $(html).find('.full-start-new__buttons, .full-start__buttons').append(btn);
         };
 
-        this.openPage = function (movie) {
-            var title = movie.title || movie.name;
-            
-            // Створюємо власну активність (сторінку)
+        this.open = function (movie) {
             Lampa.Activity.push({
-                title: 'Козак ТВ: ' + title,
-                component: 'kozak_search', 
+                title: 'Козак ТВ',
+                component: 'online',
                 movie: movie,
+                page: 1,
                 onRender: function(object) {
-                    var scroll = new Lampa.Scroll({mask: true, over: true});
-                    var files = new Lampa.Explorer(object);
-                    
-                    // Малюємо індикатор завантаження
-                    scroll.append(Lampa.Template.get('loader'));
-                    
-                    var url = 'https://cors.lampac.sh/https://api.alloha.tv/?token=044417740f9350436d7a71888e5d61&name=' + encodeURIComponent(title);
-
-                    var network = new Lampa.Reguest();
-                    network.silent(url, function (res) {
-                        scroll.clear(); // Прибираємо завантаження
+                    object.search = function() {
+                        object.loading(true);
+                        var title = movie.title || movie.name;
                         
-                        if (res && res.data && res.data.iframe) {
-                            var item = Lampa.Template.get('online_item', {
-                                title: res.data.name || title,
-                                quality: 'HD'
-                            });
+                        // Використовуємо стабільний шлюз, який ми бачили в check.sh
+                        var url = 'http://golampaua.mooo.com/lite/online?id=' + movie.id + '&title=' + encodeURIComponent(title);
 
-                            item.on('hover:enter click', function () {
-                                var video = res.data.iframe;
-                                if (video.indexOf('//') === 0) video = 'https:' + video;
-                                Lampa.Player.play({ url: video, title: title });
-                            });
-
-                            scroll.append(item);
-                        } else {
-                            scroll.append('<div class="empty">Нічого не знайдено в базі Козак ТВ</div>');
-                        }
-                        Lampa.Controller.enable('content');
-                    }, function () {
-                        scroll.clear();
-                        scroll.append('<div class="empty">Помилка мережі (Перевірте проксі)</div>');
-                    });
-
-                    return scroll.render();
+                        // Використовуємо тунель, щоб обійти порожню сторінку
+                        Lampa.NativeWsClient.send('proxy', { url: url, method: 'GET' }, function(res) {
+                            object.loading(false);
+                            if (res && res.length) {
+                                var items = res.map(function(i) {
+                                    return {
+                                        title: i.name || i.title || title,
+                                        file: i.url || i.video || i.file,
+                                        quality: i.quality || 'HD',
+                                        info: 'Козак ТВ'
+                                    };
+                                });
+                                object.draw(items, {
+                                    onEnter: function(item) {
+                                        Lampa.Player.play({url: item.file, title: item.title});
+                                    }
+                                });
+                            } else {
+                                object.empty(); // Малює системне вікно "Порожньо"
+                            }
+                        }, function() {
+                            object.loading(false);
+                            object.empty();
+                            Lampa.Noty.show('Помилка з’єднання. Перевірте Парсер.');
+                        });
+                    };
+                    object.search();
                 }
             });
         };
