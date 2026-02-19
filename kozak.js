ca515bb4719ffddbc56db36286b99ab1655f933f
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 03:15:34 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -15,51 +15,59 @@
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
             var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #ffde1a !important; color: #000 !important; border: 2px solid #fff;"><span>КОЗАК ТВ</span></div>');
-            btn.on('hover:enter click', function () { _this.open(data.movie); });
+            btn.on('hover:enter click', function () { _this.search(data.movie); });
             $(html).find('.full-start-new__buttons, .full-start__buttons').append(btn);
         };
 
-        this.open = function (movie) {
-            Lampa.Activity.push({
-                title: 'Козак ТВ',
-                component: 'online',
-                movie: movie,
-                page: 1,
-                onRender: function(object) {
-                    object.search = function() {
-                        object.loading(true);
-                        var title = movie.title || movie.name;
-                        
-                        // Використовуємо стабільний шлюз, який ми бачили в check.sh
-                        var url = 'http://golampaua.mooo.com/lite/online?id=' + movie.id + '&title=' + encodeURIComponent(title);
+        this.search = function (movie) {
+            var title = movie.title || movie.name;
+            var clean_title = title.replace(/[:]/g, ''); // Деякі бази не люблять двокрапки
+            
+            Lampa.Noty.show('Пошук у базах...');
 
-                        // Використовуємо тунель, щоб обійти порожню сторінку
-                        Lampa.NativeWsClient.send('proxy', { url: url, method: 'GET' }, function(res) {
-                            object.loading(false);
-                            if (res && res.length) {
-                                var items = res.map(function(i) {
-                                    return {
-                                        title: i.name || i.title || title,
-                                        file: i.url || i.video || i.file,
-                                        quality: i.quality || 'HD',
-                                        info: 'Козак ТВ'
-                                    };
-                                });
-                                object.draw(items, {
-                                    onEnter: function(item) {
-                                        Lampa.Player.play({url: item.file, title: item.title});
-                                    }
-                                });
-                            } else {
-                                object.empty(); // Малює системне вікно "Порожньо"
+            // Використовуємо Alloha через corsproxy, оскільки ти кажеш, що проксі не потрібен
+            var url = 'https://api.alloha.tv/?token=044417740f9350436d7a71888e5d61&name=' + encodeURIComponent(clean_title);
+
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json',
+                timeout: 5000,
+                success: function (res) {
+                    if (res && res.data && res.data.iframe) {
+                        var items = [{
+                            title: res.data.name || title,
+                            subtitle: 'Якість: HD',
+                            file: res.data.iframe
+                        }];
+
+                        Lampa.Select.show({
+                            title: 'Знайдено на Козак ТВ',
+                            items: items,
+                            onSelect: function (item) {
+                                var video = item.file;
+                                if (video.indexOf('//') === 0) video = 'https:' + video;
+                                Lampa.Player.play({ url: video, title: item.title });
                             }
-                        }, function() {
-                            object.loading(false);
-                            object.empty();
-                            Lampa.Noty.show('Помилка з’єднання. Перевірте Парсер.');
                         });
-                    };
-                    object.search();
+                    } else {
+                        // Якщо Alloha не знайшла, спробуємо резерв (Rezka) через прямий плеєр
+                        _this.playDirect(clean_title);
+                    }
+                },
+                error: function () {
+                    _this.playDirect(clean_title);
+                }
+            });
+        };
+
+        this.playDirect = function(title) {
+            var rezka_url = 'https://voidboost.net/embed/movie?title=' + encodeURIComponent(title);
+            Lampa.Select.show({
+                title: 'Резервне джерело',
+                items: [{title: 'Дивитися на Rezka', file: rezka_url}],
+                onSelect: function(item) {
+                    Lampa.Player.play({ url: item.file, title: title });
                 }
             });
         };
