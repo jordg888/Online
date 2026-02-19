6d0a0e6ee56c592e640f525f08d6db74237a1ddb
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 02:04:37 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -14,7 +14,7 @@
 
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
-            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #ffde1a !important; color: #000 !important; border: 1px solid #fff;"><span>КОЗАК ТВ</span></div>');
+            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #ffde1a !important; color: #000 !important; border: 2px solid #fff;"><span>КОЗАК ТВ</span></div>');
             btn.on('hover:enter click', function () { _this.openPage(data.movie); });
             $(html).find('.full-start-new__buttons, .full-start__buttons').append(btn);
         };
@@ -22,54 +22,47 @@
         this.openPage = function (movie) {
             var title = movie.title || movie.name;
             
+            // Створюємо власну активність (сторінку)
             Lampa.Activity.push({
-                title: 'Козак ТВ',
-                component: 'online',
+                title: 'Козак ТВ: ' + title,
+                component: 'kozak_search', 
                 movie: movie,
-                page: 1,
                 onRender: function(object) {
-                    // Створюємо логіку пошуку всередині сторінки
-                    object.search = function() {
-                        object.loading(true);
-                        
-                        // Використовуємо шлях, який ви знайшли в робочому плагіні
-                        var url = 'http://golampaua.mooo.com/lite/online?id=' + movie.id + 
-                                  '&title=' + encodeURIComponent(title) + 
-                                  '&year=' + (movie.release_date || '').slice(0,4);
-
-                        // Використовуємо NativeWsClient (тунель через WebSocket)
-                        // Це дозволяє обійти помилку "Неможливо відтворити посилання"
-                        Lampa.NativeWsClient.send('proxy', {
-                            url: url,
-                            method: 'GET'
-                        }, function(res) {
-                            if (res && res.length) {
-                                var items = res.map(function(i) {
-                                    return {
-                                        title: i.name || i.title || title,
-                                        file: i.url || i.video || i.file,
-                                        quality: i.quality || 'HD',
-                                        info: 'UA/INT'
-                                    };
-                                });
-                                // Малюємо список на сторінці
-                                object.draw(items, {
-                                    onEnter: function(item) {
-                                        Lampa.Player.play({url: item.file, title: item.title});
-                                    }
-                                });
-                            } else {
-                                object.empty(); // Покаже "Тут порожньо", якщо нічого не знайдено
-                            }
-                            object.loading(false);
-                        }, function() {
-                            Lampa.Noty.show('Помилка тунелювання. Перевірте мережу.');
-                            object.loading(false);
-                        });
-                    };
+                    var scroll = new Lampa.Scroll({mask: true, over: true});
+                    var files = new Lampa.Explorer(object);
                     
-                    // Запускаємо пошук після того, як сторінка намальована
-                    object.search();
+                    // Малюємо індикатор завантаження
+                    scroll.append(Lampa.Template.get('loader'));
+                    
+                    var url = 'https://cors.lampac.sh/https://api.alloha.tv/?token=044417740f9350436d7a71888e5d61&name=' + encodeURIComponent(title);
+
+                    var network = new Lampa.Reguest();
+                    network.silent(url, function (res) {
+                        scroll.clear(); // Прибираємо завантаження
+                        
+                        if (res && res.data && res.data.iframe) {
+                            var item = Lampa.Template.get('online_item', {
+                                title: res.data.name || title,
+                                quality: 'HD'
+                            });
+
+                            item.on('hover:enter click', function () {
+                                var video = res.data.iframe;
+                                if (video.indexOf('//') === 0) video = 'https:' + video;
+                                Lampa.Player.play({ url: video, title: title });
+                            });
+
+                            scroll.append(item);
+                        } else {
+                            scroll.append('<div class="empty">Нічого не знайдено в базі Козак ТВ</div>');
+                        }
+                        Lampa.Controller.enable('content');
+                    }, function () {
+                        scroll.clear();
+                        scroll.append('<div class="empty">Помилка мережі (Перевірте проксі)</div>');
+                    });
+
+                    return scroll.render();
                 }
             });
         };
