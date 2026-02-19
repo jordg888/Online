165337a08495f63dd344ca0b6bc3953de1beebed
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-20 00:10:40 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -4,31 +4,28 @@
     function KozakPlugin() {
         Lampa.Listener.follow('full', function (e) {
             if (e.type === 'complite') {
-                setTimeout(function() {
-                    var render = e.object.activity.render();
-                    var container = render.find('.full-start-new__buttons, .full-start__buttons');
+                var render = e.object.activity.render();
+                // Шукаємо контейнер для кнопок
+                var container = render.find('.full-start-new__buttons, .full-start__buttons');
+                
+                if (container.length && !container.find('.kozak-button').length) {
+                    // Створюємо кнопку з примусовими стилями для видимості
+                    var btn = $('<div class="full-start__button selector kozak-button" style="background: #24b47e; color: #fff; opacity: 1 !important; display: flex !important;"><span>КОЗАК ТВ</span></div>');
                     
-                    if (container.length && !container.find('.kozak-button').length) {
-                        var btn = $('<div class="full-start__button selector kozak-button"><span>КОЗАК ТВ (UA)</span></div>');
-                        
-                        btn.on('click', function () {
-                            var movie = e.data.movie;
-                            
-                            // Викликаємо вікно вибору балансерів через універсальний онлайн-модуль
-                            Lampa.Component.add('online', {
-                                title: movie.title || movie.name,
-                                url: '', 
-                                movie: movie,
-                                // Цей метод змушує Лампу шукати по всіх активних плагінах онлайн-перегляду
-                                onBack: function() {
-                                    Lampa.Activity.backward();
-                                }
-                            });
+                    btn.on('click', function () {
+                        // Викликаємо пошук по всіх встановлених балансерах
+                        Lampa.Component.add('online', {
+                            title: e.data.movie.title || e.data.movie.name,
+                            url: '',
+                            movie: e.data.movie,
+                            onBack: function() {
+                                Lampa.Activity.backward();
+                            }
                         });
+                    });
 
-                        container.append(btn);
-                    }
-                }, 100);
+                    container.append(btn);
+                }
             }
         });
     }
