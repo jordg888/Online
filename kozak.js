5238f9185f4a3a6688364bc75be9258bf22549d1
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 11:16:52 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -1,72 +1,30 @@
-(function () {
-    'use strict';
-
-    function KozakTiv() {
-        var _this = this;
-
-        this.init = function () {
-            Lampa.Listener.follow('full', function (e) {
-                if (e.type === 'complite' || e.type === 'ready') {
-                    _this.render(e.data, e.object.activity.render());
-                }
-            });
-        };
-
-        this.render = function (data, html) {
-            $('.lampa-kozak-btn').remove();
-            // Створюємо кнопку з максимальною сумісністю стилів
-            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #27ae60 !important; color: #fff !important; border-radius: 8px; margin-top: 10px; display: flex; align-items: center; justify-content: center; height: 3.5em; cursor: pointer;"><span>КОЗАК ТВ</span></div>');
-            
-            btn.on('click', function () {
-                _this.search(data.movie);
-            });
-
-            // Шукаємо куди вставити (перебираємо всі варіанти контейнерів)
-            var container = $(html).find('.full-start-new__buttons, .full-start__buttons, .full-start__btns');
-            
-            if (container.length > 0) {
-                container.append(btn);
-            } else {
-                // Якщо не знайшли спец. контейнер, тулимо після опису
-                $(html).find('.full-start__description').after(btn);
-            }
-        };
-
-        this.search = function (movie) {
+this.search = function (movie) {
             var title = movie.title || movie.name;
             Lampa.Noty.show('Шукаю на Козаку: ' + title);
 
+            // Використовуємо http замість https як запасний варіант, якщо сертифікат битий
             var api_url = 'https://api.alloha.tv/?token=044417740f9350436d7a71888e5d61&name=' + encodeURIComponent(title);
 
-            // Використовуємо найнадійніший метод запиту для твоєї збірки
-            var network = Lampa.Reguest || Lampa.Network || Lampa.Request;
-            
-            if (network && typeof network.native === 'function') {
-                network.native(api_url, function(res) {
+            $.ajax({
+                url: api_url,
+                method: 'GET',
+                dataType: 'json',
+                success: function(res) {
                     var video = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
                     if (video) {
                         if (video.indexOf('//') === 0) video = 'https:' + video;
                         Lampa.Player.play({ url: video, title: title });
                     } else {
-                        Lampa.Noty.show('Відео не знайдено в базі');
+                        Lampa.Noty.show('Відео не знайдено');
                     }
-                }, function() {
-                    Lampa.Noty.show('Помилка запиту до API');
-                });
-            } else {
-                // Резервний метод через прямий AJAX
-                $.getJSON(api_url, function(res) {
-                    var video = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
-                    if (video) {
-                        if (video.indexOf('//') === 0) video = 'https:' + video;
-                        Lampa.Player.play({ url: video, title: title });
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    if (errorThrown === "" || textStatus === "error") {
+                        Lampa.Noty.show('Проблема з сертифікатом API. Відкрийте api.alloha.tv у браузері та натисніть "Дозволити"');
+                    } else {
+                        Lampa.Noty.show('Помилка мережі: ' + textStatus);
                     }
-                }).fail(function() {
-                    Lampa.Noty.show('Браузер заблокував запит (CORS)');
-                });
-            }
+                    console.log('Kozak Error Details:', textStatus, errorThrown);
+                }
+            });
         };
-    }
-
-    if (window.Lampa) new KozakTiv().init();
-})();
