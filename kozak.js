06c13fbc475a2282f3c7532bdd3c4b823d56263b
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 11:22:19 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -14,7 +14,7 @@
 
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
-            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #e67e22 !important; color: #fff !important; border-radius: 8px; margin-top: 10px; display: flex; align-items: center; justify-content: center; height: 3.5em; cursor: pointer;"><span>КОЗАК ТВ: ДИВИТИСЬ</span></div>');
+            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #e67e22 !important; color: #fff !important; border-radius: 8px; margin-top: 10px; display: flex; align-items: center; justify-content: center; height: 3.5em; cursor: pointer;"><span>КОЗАК ТВ: ГРАТИ</span></div>');
             
             btn.on('click', function () {
                 _this.search(data.movie);
@@ -27,44 +27,20 @@
 
         this.search = function (movie) {
             var title = movie.title || movie.name;
-            var year = movie.release_date ? movie.release_date.split('-')[0] : (movie.first_air_date ? movie.first_air_date.split('-')[0] : '');
-            
-            Lampa.Noty.show('Шукаю плеєри для: ' + title);
+            // Створюємо пряме посилання на віджет Kinobox
+            // Це найнадійніший спосіб, який не блокується по CORS
+            var kinobox_url = 'https://kinobox.tv/embed/tmdb/' + movie.id;
 
-            // Використовуємо Kinobox API (не потребує токенів)
-            // Шукаємо за назвою та роком для точності
-            var search_url = 'https://kinobox.tv/api/players?title=' + encodeURIComponent(title) + (year ? '&year=' + year : '');
+            Lampa.Noty.show('Запускаю Козак-Плеєр...');
 
-            $.ajax({
-                url: search_url,
-                method: 'GET',
-                dataType: 'json',
-                success: function(res) {
-                    // Kinobox повертає масив знайдених плеєрів
-                    if (res && res.length > 0) {
-                        // Беремо перший знайдений робочий плеєр (зазвичай він найстабільніший)
-                        var video = res[0].iframeUrl || res[0].link;
-
-                        if (video) {
-                            if (video.indexOf('//') === 0) video = 'https:' + video;
-                            
-                            Lampa.Noty.show('Знайдено ' + res.length + ' варіантів. Запускаю...');
-                            
-                            Lampa.Player.play({
-                                url: video,
-                                title: title
-                            });
-                        } else {
-                            Lampa.Noty.show('Посилання на відео порожнє');
-                        }
-                    } else {
-                        Lampa.Noty.show('Нічого не знайдено в базах');
-                    }
-                },
-                error: function() {
-                    Lampa.Noty.show('Помилка мережі або блокування');
-                }
+            // Викликаємо вбудований плеєр Lampa з посиланням на віджет
+            Lampa.Player.play({
+                url: kinobox_url,
+                title: title
             });
+            
+            // На випадок, якщо потрібен вибір джерела всередині плеєра
+            Lampa.Noty.show('Якщо не вантажить, перевірте з\'єднання');
         };
     }
 
