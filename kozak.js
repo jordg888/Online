98a337da0a5d8bfffe1f89fffca0305497eaf8f2
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-20 00:06:46 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -1,19 +1,38 @@
-// Додай цей блок усередину функції click у твоєму kozak.js
-btn.addEventListener('click', function () {
-    var movie = e.data.movie;
-    
-    // Використовуємо метод Lampa для відкриття пошуку по балансерах
-    // Це дозволить кнопці "КОЗАК ТВ" викликати список доступних джерел
-    Lampa.Api.search({
-        query: movie.title || movie.name
-    }).then(function(){
-        // Або відкриваємо специфічний плеєр, який вміє обходити блокування
-        var videoUrl = 'https://vjs.su/embed/tmdb/' + movie.id;
-        
-        Lampa.Player.run({
-            url: videoUrl,
-            title: movie.title
-        });
-    });
-});
+(function () {
+    'use strict';
 
+    function KozakPlugin() {
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type === 'complite') {
+                setTimeout(function() {
+                    var render = e.object.activity.render();
+                    var container = render.find('.full-start-new__buttons, .full-start__buttons');
+                    
+                    if (container.length && !container.find('.kozak-button').length) {
+                        var btn = $('<div class="full-start__button selector kozak-button"><span>КОЗАК ТВ (UA)</span></div>');
+                        
+                        btn.on('click', function () {
+                            var movie = e.data.movie;
+                            
+                            // Викликаємо вікно вибору балансерів через універсальний онлайн-модуль
+                            Lampa.Component.add('online', {
+                                title: movie.title || movie.name,
+                                url: '', 
+                                movie: movie,
+                                // Цей метод змушує Лампу шукати по всіх активних плагінах онлайн-перегляду
+                                onBack: function() {
+                                    Lampa.Activity.backward();
+                                }
+                            });
+                        });
+
+                        container.append(btn);
+                    }
+                }, 100);
+            }
+        });
+    }
+
+    if (window.Lampa) KozakPlugin();
+    else window.addEventListener('lampa_ready', KozakPlugin);
+})();
