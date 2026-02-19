19a3f78c6054d809b2e158a13fdcca81be8593fd
jordg888 <81681003+jordg888@users.noreply.github.com>
2026-02-19 11:10:16 +0200

    Update kozak.js

--- kozak.js
+++ kozak.js
@@ -14,7 +14,7 @@
 
         this.render = function (data, html) {
             $('.lampa-kozak-btn').remove();
-            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #27ae60 !important; color: #fff !important; border-radius: 5px; margin-top: 10px;"><span>КОЗАК ТВ</span></div>');
+            var btn = $('<div class="full-start__button selector lampa-kozak-btn" style="background: #27ae60 !important; color: #fff !important; border-radius: 5px; margin-top: 10px; width: 100%; text-align: center;"><span>КОЗАК ТВ</span></div>');
             
             btn.on('click', function () {
                 _this.search(data.movie);
@@ -27,45 +27,26 @@
 
         this.search = function (movie) {
             var title = movie.title || movie.name;
-            Lampa.Noty.show('Шукаю через ' + title + '...');
+            Lampa.Noty.show('Шукаю: ' + title);
 
+            // Використовуємо https протокол обов'язково
             var api_url = 'https://api.alloha.tv/?token=044417740f9350436d7a71888e5d61&name=' + encodeURIComponent(title);
 
-            // Шукаємо робочий метод запиту в надрах Лампи
-            // Спробуємо по черзі всі відомі варіанти
-            var network = Lampa.Network || Lampa.Reguest || Lampa.Request;
-            var sendNative = network ? (network.native || network.get) : null;
-
-            if (typeof sendNative === 'function') {
-                sendNative(api_url, function(res) {
-                    console.log('Kozak: Знайдено!', res);
-                    var video_url = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
-
-                    if (video_url) {
-                        if (video_url.indexOf('//') === 0) video_url = 'https:' + video_url;
-                        Lampa.Player.play({ url: video_url, title: title });
+            // Використовуємо Lampa.Reguest.native як основний метод, бо він вже у тебе "завівся"
+            // Але додаємо обробку помилки блокування
+            if (window.Lampa && Lampa.Reguest && Lampa.Reguest.native) {
+                Lampa.Reguest.native(api_url, function(res) {
+                    var video = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
+                    if (video) {
+                        if (video.indexOf('//') === 0) video = 'https:' + video;
+                        Lampa.Player.play({ url: video, title: title });
                     } else {
-                        Lampa.Noty.show('Відео не знайдено');
+                        Lampa.Noty.show('Нічого не знайдено');
                     }
-                }, function() {
-                    Lampa.Noty.show('Помилка мережі (API)');
-                });
-            } else {
-                // Якщо Лампа забарикадувала всі двері, спробуємо старий добрий jQuery (але може бути CORS)
-                console.log('Kozak: Використовую запасний метод (jQuery)');
-                $.ajax({
-                    url: api_url,
-                    dataType: 'json',
-                    success: function(res) {
-                        var video_url = res.data ? (res.data.iframe_url || res.data.iframe) : (res.iframe_url || res.iframe);
-                        if (video_url) {
-                            if (video_url.indexOf('//') === 0) video_url = 'https:' + video_url;
-                            Lampa.Player.play({ url: video_url, title: title });
-                        }
-                    },
-                    error: function() {
-                        Lampa.Noty.show('Не вдалося виконати запит');
-                    }
+                }, function(err) {
+                    // Якщо ERR_BLOCKED_BY_CLIENT, показуємо підказку
+                    console.error('Kozak Blocked:', err);
+                    Lampa.Noty.show('Запит заблоковано браузером. Вимкніть AdBlock');
                 });
             }
         };
